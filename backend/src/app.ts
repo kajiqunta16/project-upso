3b567897c0c57a9f7d40659f7dc623674ea0f652
kajiqunta16 <mujtuba.nin@gmail.com>
2026-03-29 07:09:28 +0500

    installation

---
+++ backend/src/app.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+app.use(express.json());
+
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("API running with TypeScript 🚀");
+});
+
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
